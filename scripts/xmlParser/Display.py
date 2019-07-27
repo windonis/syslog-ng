cfa5df84327e8c75957701e00d367185b5a41b5d
windonis <enis.kollugil@itu.edu.tr>
2019-07-27 13:13:59 -0700

    trying .ym creator

--- scripts/xmlParser/Display.py
+++ scripts/xmlParser/Display.py
@@ -8,12 +8,12 @@ graph.EdgeAdder()
 
 def Nameless (ruleNumber):
     store = []
-    for i in (parser.G.edges.data('Nweight')):
-        if ( i[0] == ruleNumber and i[2] is not None):
-            lst = list(i)
-            lst[0] = parser.G.node[ruleNumber]['lhs']
-            store.append(lst)
+    for i in parser.xmltoobject():
+        if ( i.number == ruleNumber):
+            store.append(i.lhs)
+            store.append(i.rhs)
     return(store)
+
 
 beginNode = []
 
@@ -22,33 +22,30 @@ for i in parser.G.nodes.data('lhs'):
         count = i[0]
 
 for x in list(nx.dfs_edges(parser.G, source=count)):
-    if x[1] == "LL_CONTEXT_DESTINATION":
+    if x[1] == 'LL_CONTEXT_DESTINATION':
         for i in Nameless(x[0]):
-            if i[1] != "LL_CONTEXT_DESTINATION":
+            if i[1] != 'LL_CONTEXT_DESTINATION':
                 beginNode.append(i[1])
-    elif x[1] == "LL_CONTEXT_SOURCE":
-        for i in Nameless(x[0]):
-            if i[1] != "LL_CONTEXT_SOURCE":
-                beginNode.append(i[1])
-'''
+    elif x[1] == 'LL_CONTEXT_SOURCE':
+        if i[1] != 'LL_CONTEXT_SOURCE':
+            beginNode.append(i[1])
+
 for i in beginNode:
-    #print(i) ---> http_destination
     for x in parser.G.nodes.data('lhs'):
         if x[1] == i:
-            #print(x[0]) ---> 4
             for part in (list(nx.dfs_edges(parser.G, source=x[0], depth_limit=0))):
                 if isinstance(part[1], int) is True:
-                    if part[1] > x[0]:
-                        print(list(nx.dfs_tree(parser.G, source=part[1], depth_limit=0)))
-'''
-'''
-                            if isinstance(z[1], int) is True:
-                                returnValue = Nameless(z[1])
-                                print("{}:{}".format(returnValue[0][1],returnValue[2][1]))
-'''
+                    if part[1] > x[0]: 
+                        if len(list(nx.dfs_edges(parser.G, source=part[1], depth_limit=0))) != 0:
+                            for f in (list(nx.dfs_edges(parser.G, source=part[1], depth_limit=0))):
+                                returnValue = Nameless(f[1])
+                                print("{}:{}".format(returnValue[1][0], returnValue[1][2]))
 
 
-test = parser.xmltoobject()
-for i in test:
-    if i.number == 61:
-        print(i.rhs)
+#abbas = Nameless(61)
+#print(abbas)
+
+'''
+for a in (list(nx.dfs_edges(parser.G, source=8, depth_limit=0))):
+    print(a)
+'''
